5d3b9fd668b47024f135c2cfce9c6a0509d470d9
Alex Kling <momiji.kling@gmail.com>
2025-12-14 09:23:25 +0100

    init Next.JS and Framer-Motion

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # DaisyWidget
+
+### Definition
+
+**Widget**: element of interaction, such as label, buttons and checkbox ; Displays information arrangement changeable by the user, such as a window or text box, and may be enabled to respond to events
+
+=> small GUI appliction that provides some visual information and or easy access to frequently used functions, hosted by a widget engine
+
+
+### Technologies
+
+- **Tailwind CSS**: CSS tookit
+- **Next.JS**: 
+- **framer-motion**: graphical animation framework, good for smooth transitions
+- **shadcn/ui**: component library
+
+
+### Benchmark
+
---
+++ daisy-widget/app/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+export default function Home() {
+	return (
+	<>
+		<main className='flex min-h-screen flex-col items-center justify-center'>
+			<motion.div
+				className='text-4xl font-fbold mb-8'
+				initial={{opacity: 0, y: -50}}
+				animate={{opacity: 1, y: 0}}
+				transition={{duration: 0.5}}>
+					Welcome to Framer Motion
+			</motion.div>
+
+			<motion.button
+				whileHover={{scale:1.1}}
+				whileTap={{scale: 0.9}}
+				className="px-4 py-2 bg-blue-500 text-white rounded-md">
+					<Link href='/about'>Click Me</Link>
+			</motion.button>
+		</main>
+	</>
+	);
+}
